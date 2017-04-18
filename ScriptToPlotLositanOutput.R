c12999b942e1dd983b80cc0271e4ddf31e98f5a1
mcadamme <mcadamme@msu.edu>
2017-04-18 10:26:31 -0400

    Add files via upload
    Added Venn Diagram plot code

--- ScriptToPlotLositanOutput.R
+++ ScriptToPlotLositanOutput.R
@@ -18,7 +18,7 @@ Corner_text <- function(text, location=location){
   legend(location,legend=text, bty ="n", pch=NA, cex=4) 
 }
 
-png('output.png', units = "px", height = 1200, width = 800)
+png(file = "SNPoutliersByYearComparison_Lositan.png", units = "px", height = 1200, width = 800)
 par(mfrow=c(3,1))
 par(mar = c(5,6,4,1))
 plot1 <- plot(-10,ylim=c(0,0.4),xlim=c(0.06,0.65), cex.lab=3, cex.axis=2.5, xlab='He', ylab='Fst')
@@ -37,7 +37,8 @@ plot_ci('FieldHvirReduced_Fst_2007thru2012_confint.txt', 'red')
 
 dev.off()
 
-#for all comparisons
+#Getting VennDiagram & list of unique contigs with outliers for all comparisons
+library("VennDiagram")
 
 snp_code <- read.table("SNPInfo.txt", head=F)
 
@@ -46,27 +47,31 @@ Hv1997thru2012 <- read.table("FieldHvirReduced_Fst_1997thru2012.txt", header = T
 Hv1997thru2012 <- cbind(Hv1997thru2012,snp_code)
 sub_Hv1997thru2012 <- subset(Hv1997thru2012, Pval >= 0.99)
 uniqContigs_1997thr2012 <- unique(sub_Hv1997thru2012$V1)
-uniqContigs_1997thr2012
+length(uniqContigs_1997thr2012)
 
 #Fst Outliers 1997-2007
 Hv1997thru2007 <- read.table("FieldHvirReduced_Fst_1997thru2007.txt", header = T)
 Hv1997thru2007 <- cbind(Hv1997thru2007, snp_code)
 sub_Hv1997thru2007 <- subset(Hv1997thru2007, Pval >= 0.99)
 uniqContigs_1997thru2007 <- unique(sub_Hv1997thru2007$V1)
-uniqContigs_1997thru2007
+length(uniqContigs_1997thru2007)
 
 #Fst Outliers 2007-2012
 Hv2007thru2012 <- read.table("FieldHvirReduced_Fst_2007thru2012.txt", header = T)
 Hv2007thru2012 <- cbind(Hv2007thru2012, snp_code)
 sub_Hv2007thru2012 <- subset(Hv2007thru2012, Pval >= 0.99)
 uniqContigs_2007thru2012 <- unique(sub_Hv2007thru2012$V1)
-uniqContigs_2007thru2012
+length(uniqContigs_2007thru2012)
 
 #Getting Venn Diagram of overlapping scaffolds
-overlap <- calculate.overlap(x = list( "1997 and 2007" = sub_Hv1997thru2007$V1, "1997 and 2012" = sub_Hv1997thru2012$V1,
-                                       "2007 and 2012" = sub_Hv2007thru2012$V1))
+overlap <- calculate.overlap(x = list( "1997 and 2007" = unique(sub_Hv1997thru2007$V1), "1997 and 2012" = unique(sub_Hv1997thru2012$V1),
+                                       "2007 and 2012" = unique(sub_Hv2007thru2012$V1)))
 
 unlist(lapply(overlap, length))
 
-draw.triple.venn(201,184,35,75,15,15,6, category = c("1997 and 2007", "1997 and 2012", "2007 and 2012"))
+#vector of plotted values comes from above overlap output.  For example, 190 = size of first circle and comes from 120 + 57 + 11 + 2
+png(file = "VennDiagram_PlotContigsWithOutlierSNPs.png", units = "px", height = 800, width = 1200)
+draw.triple.venn(190,170,33,59,9,13,2, category = c("1997 and 2007", "1997 and 2012", "2007 and 2012"),
+                 fill = c("orange", "blue", "green"), cex = 2, cat.cex = 2.5)
 
+dev.off()
