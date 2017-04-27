e92a671729bdb1e58a11fe949715e4b2eab7780f
mcadamme <mcadamme@msu.edu>
2017-04-27 11:49:21 -0400

    Add files via upload
    added code to pull out functionally annotated genes from GTF file that are near SNP outliers

--- ScriptToPlotLositanOutput.R
+++ ScriptToPlotLositanOutput.R
@@ -41,31 +41,47 @@ dev.off()
 library("VennDiagram")
 
 snp_code <- read.table("SNPInfo.txt", head=F)
+colnames(snp_code) <- c("Contig","StartPos")
+
+#loading GTF file with structural annotation
+GTF <- read.table("H.virescens_OGS1_cleaned.gtf", header = F)
+colnames(GTF) <- c("Contig","Prog", "Type", "StartPos", "StopPos", "Score", 
+                   "Strand", "Frame", "Attr1", "Attr2", "Attr3", "Attr4", "Attr5", "Attr6", 
+                   "Attr7","Attr8","Attr9")
+
+sub_GTF <- GTF[,c(1,4,5,13)]
 
 #Fst Outliers 1997-2012
 Hv1997thru2012 <- read.table("FieldHvirReduced_Fst_1997thru2012.txt", header = T)
 Hv1997thru2012 <- cbind(Hv1997thru2012,snp_code)
 sub_Hv1997thru2012 <- subset(Hv1997thru2012, Pval >= 0.99)
-uniqContigs_1997thr2012 <- unique(sub_Hv1997thru2012$V1)
+uniqContigs_1997thr2012 <- unique(sub_Hv1997thru2012$Contig)
 length(uniqContigs_1997thr2012)
 
 #Fst Outliers 1997-2007
 Hv1997thru2007 <- read.table("FieldHvirReduced_Fst_1997thru2007.txt", header = T)
 Hv1997thru2007 <- cbind(Hv1997thru2007, snp_code)
 sub_Hv1997thru2007 <- subset(Hv1997thru2007, Pval >= 0.99)
-uniqContigs_1997thru2007 <- unique(sub_Hv1997thru2007$V1)
+uniqContigs_1997thru2007 <- unique(sub_Hv1997thru2007$Contig)
 length(uniqContigs_1997thru2007)
 
 #Fst Outliers 2007-2012
 Hv2007thru2012 <- read.table("FieldHvirReduced_Fst_2007thru2012.txt", header = T)
 Hv2007thru2012 <- cbind(Hv2007thru2012, snp_code)
 sub_Hv2007thru2012 <- subset(Hv2007thru2012, Pval >= 0.99)
-uniqContigs_2007thru2012 <- unique(sub_Hv2007thru2012$V1)
+uniqContigs_2007thru2012 <- unique(sub_Hv2007thru2012$Contig)
 length(uniqContigs_2007thru2012)
 
+#Note Heterozygosity values are exceeding 0.5 for some of these SNPs. 
+#I understand that this may be due to the way that Lositan calculates heterozygosity.
+#In Lositan "Heterozygosity" is the probability of choosing two alleles at random that are different, one from each deme, 
+#rather than the prob of picking two alleles at random that are different irrespective of deme. With two demes it can 
+#reach 1 when FST is 1. So the reason why it is so high is that we are probably looking at populations that look 
+#"inbred" at these loci.
+
 #Getting Venn Diagram of overlapping scaffolds
-overlap <- calculate.overlap(x = list( "1997 and 2007" = unique(sub_Hv1997thru2007$V1), "1997 and 2012" = unique(sub_Hv1997thru2012$V1),
-                                       "2007 and 2012" = unique(sub_Hv2007thru2012$V1)))
+overlap <- calculate.overlap(x = list( "1997 and 2007" = unique(sub_Hv1997thru2007$Contig), "1997 and 2012" = unique(sub_Hv1997thru2012$Contig),
+                                       "2007 and 2012" = unique(sub_Hv2007thru2012$Contig)))
 
 unlist(lapply(overlap, length))
 
@@ -75,3 +91,26 @@ draw.triple.venn(190,170,33,59,9,13,2, category = c("1997 and 2007", "1997 and 2
                  fill = c("orange", "blue", "green"), cex = 2, cat.cex = 2.5)
 
 dev.off()
+
+#merging significant SNPs with GO terms
+merged_sig_1997thru2012 <- merge(sub_Hv1997thru2012, sub_GTF, by = "Contig")
+merged_sig_1997thru2007 <- merge(sub_Hv1997thru2007, sub_GTF, by = "Contig")
+merged_sig_2007thru2012 <- merge(sub_Hv2007thru2007, sub_GTF, by = "Contig")
+
+#Getting genes within 10kb of SNP outliers
+#1997thru2012
+subLow_merged_sig_1997thru2012 <- subset(merged_sig_1997thru2012, StartPos.y > merged_sig_1997thru2012$StartPos.x - 10000)
+suball_merged_sig_1997thru2012 <- subset(subLow_merged_sig_1997thru2012, StartPos.y < subLow_merged_sig_1997thru2012$StartPos.x + 10000)
+write.table(suball_merged_sig_1997thru2012, file = "JamgModels_forSigSNPs_1997thru2012.txt")
+
+#1997thru2007
+subLow_merged_sig_1997thru2007 <- subset(merged_sig_1997thru2007, StartPos.y > merged_sig_1997thru2007$StartPos.x - 10000)
+suball_merged_sig_1997thru2007 <- subset(subLow_merged_sig_1997thru2007, StartPos.y < subLow_merged_sig_1997thru2007$StartPos.x + 10000)
+write.table(suball_merged_sig_1997thru2007, file = "JamgModels_forSigSNPs_1997thru2007.txt")
+
+#2007thru2012
+subLow_merged_sig_2007thru2012 <- subset(merged_sig_2007thru2012, StartPos.y > merged_sig_2007thru2012$StartPos.x - 10000)
+suball_merged_sig_2007thru2012 <- subset(subLow_merged_sig_2007thru2012, StartPos.y < subLow_merged_sig_2007thru2012$StartPos.x + 10000)
+write.table(suball_merged_sig_2007thru2012, file = "JamgModels_forSigSNPs_2007thru2012.txt")
+
+
